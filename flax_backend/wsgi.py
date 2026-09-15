f2c8ddfc0969aa74cd115538a93d0f5e1f9e0ee1
cadkalpana-ship-it <cadkalpana@gmail.com>
2026-09-15 14:54:16 +0530

    Add gunicorn dependency for production deployment

--- flax_backend/wsgi.py
+++ flax_backend/wsgi.py
@@ -1,8 +1,12 @@
 import os
 import pymysql
 
+# 1. Patch MySQL driver support immediately on startup
 pymysql.install_as_MySQLdb()
 
-from django.core.wsgi import get_wsgi_application
+# 2. Tell Django which settings module to target
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flax_backend.settings')
+
+# 3. NOW safely initialize the WSGI handler after environment configuration is mapped
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
